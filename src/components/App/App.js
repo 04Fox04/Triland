fd78147a86ac3ba4eeda1b26b7c9d3e7d6753a1f
Klimanova Julia <sempppai124@yandex.ru>
2023-09-22 13:02:19 +0300

    test pull request

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Routes, Route } from "react-router-dom"
 import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route>
             <Header />
+            <Footer />
           </Route>
         </Routes>
       </div>
