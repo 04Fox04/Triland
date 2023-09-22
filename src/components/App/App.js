306e7ae292888c4ec813760af0599782edc7167a
Kirill <kirill.s.o@yandex.ru>
2023-09-22 11:19:29 +0300

    test

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React, { Route } from "react"
+
+function App() {
+
+  return (
+    <div className='body'>
+      <div className='page'>
+        <Route>
+
+        </Route>
+      </div>
+    </div>
+  )
+}
+
+export default App;
