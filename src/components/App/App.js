c90aa7c89552528b076b3bf8e206d8f4236d4601
Kirill <kirill.s.o@yandex.ru>
2023-09-25 17:36:53 +0300

    adding Header and Promo blocks | fonts replaced

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../../index.css"
 import { Routes, Route } from "react-router-dom";
 import Main from "../Main/Main"
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,30 @@
 import React from "react";
+import "./Header.css"
+import Logo from "../../images/logo-white.svg"
+import PhoneWhite from "../../images/phone-white.svg"
+import { Link } from "react-router-dom";
 
-function Header () {
+function Header() {
 
     return (
-        <header>
-            <div>dgdgdg</div>
+        <header className="header">
+            <div className="header__container">
+                <Link to="/" className="header__logo-container"><img src={Logo} className="header__logo" alt="Логотип" /></Link>
+                <ul className="header__nav">
+                    <li className="header__nav-link"><Link><p className="header__link-text">О компании</p></Link></li>
+                    <li className="header__nav-link"><Link><p className="header__link-text">Услуги</p></Link></li>
+                    <li className="header__nav-link"><Link><p className="header__link-text">Лицензии</p></Link></li>
+                    <li className="header__nav-link"><Link><p className="header__link-text">Партнеры</p></Link></li>
+                    <li className="header__nav-link"><Link><p className="header__link-text">Фотогалерея</p></Link></li>
+                    <li className="header__nav-link"><Link><p className="header__link-text">Контакты</p></Link></li>
+                </ul>
+                <div className="phone__container">
+                    <img src={PhoneWhite} className="phone__image" alt="Иконка телефона"/>
+                    <p className="phone__text">
+                        <a href="tel:+7 (495)-763-3470" className="phone__text-link">+7 495 763-34-70</a>
+                    </p>
+                </div>
+            </div>
         </header>
     )
 }
