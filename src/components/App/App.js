ce98da285fa2f13bf3d9a93024a8ac157021132f
Kirill <kirill.s.o@yandex.ru>
2023-09-25 10:46:09 +0300

    test PR-3

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,19 @@
-import React from "react"
-import { Routes, Route } from "react-router-dom"
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Header from "../Header/Header";
 import Promo from "../Promo/Promo"
 
 function App() {
-
   return (
-    <div className='body'>
-      <div className='page'>
+    <div className="body">
+      <div className="page">
         <Routes>
-          <Route path="/" element={<Header />} />
-          <Route path="/" element={<Promo />} />
+          <Route to="/" element={<Header/>}/>
+          <Route to="/" element={<Promo/>}/>
         </Routes>
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
