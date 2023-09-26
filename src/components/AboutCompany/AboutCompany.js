1bd533c4be33532ae9e18b7e119c1a9dda5f0637
Kirill <kirill.s.o@yandex.ru>
2023-09-26 10:55:02 +0300

    edited code

--- src/components/AboutCompany/AboutCompany.js
+++ src/components/AboutCompany/AboutCompany.js
@@ -2,40 +2,52 @@ import React from "react";
 import "./AboutCompany.css";
 import "../../index.css";
 import { Link } from "react-router-dom";
-import ArrowSlid from "../../images/about-company/arrow.svg"
+import ArrowSlid from "../../images/about-company/arrow.svg";
 
 function AboutCompany() {
-    return (
-        <section className="about__company">
-            <div className="about__company-container">
-                <div className="about__company-info">
-                    <h2 className="section-title about__company-title">О компании</h2>
-                    <div className="about__company-border"></div>
-                    <p className="about__company-text">Основным видом деятельности компании является строительство
-                        и монтаж инженерных сетей водоснабжения, канализации, теплоснабжения жилых и промышленных зданий,
-                        а также продажа и установка автоматизированной трубопроводной арматуры от лучших производителей.</p>
-                    <p className="about__company-text">В компании работают высококвалифицированные сотрудники,
-                        специализацией которых являются сварочно-сантехнические работы.</p>
-                    <p className="about__company-text">За долгие годы работы в данной области коллектив компании
-                        «Триланд» сталкивался с множеством сложностей и при этом отлично справлялся с их выполнением.</p>
-                    <button className="about__company-button">Заказать звонок</button>
-                </div>
-                <div className="about__company-slider">
-                    {/* <img className="about__company-image" src="" alt="Фото с производства"></img> */}
-                    <div className="about__company-slid">
-                        <div className="slid__container">
-                            <h3 className="slid__text">Перейти в галерею</h3>
-                            <Link to="*">
-                                <button className="slid__button">
-                                    <img className="slid__arrow" src={ArrowSlid} alt="Стрелка"></img>
-                                </button>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <section className="about__company">
+      <div className="about__company-container">
+        <div className="about__company-info">
+          <h2 className="section-title about__company-title">О компании</h2>
+          <div className="about__company-border"></div>
+          <p className="about__company-text">
+            Основным видом деятельности компании является строительство и монтаж
+            инженерных сетей водоснабжения, канализации, теплоснабжения жилых и
+            промышленных зданий, а также продажа и установка автоматизированной
+            трубопроводной арматуры от лучших производителей.
+          </p>
+          <p className="about__company-text">
+            В компании работают высококвалифицированные сотрудники,
+            специализацией которых являются сварочно-сантехнические работы.
+          </p>
+          <p className="about__company-text">
+            За долгие годы работы в данной области коллектив компании «Триланд»
+            сталкивался с множеством сложностей и при этом отлично справлялся с
+            их выполнением.
+          </p>
+          <button className="about__company-button">Заказать звонок</button>
+        </div>
+        <div className="about__company-slider">
+          {/* <img className="about__company-image" src="" alt="Фото с производства"></img> */}
+          <div className="about__company-slid">
+            <div className="slid__container">
+              <h3 className="slid__text">Перейти в галерею</h3>
+              <Link to="*">
+                <button className="slid__button">
+                  <img
+                    className="slid__arrow"
+                    src={ArrowSlid}
+                    alt="Стрелка"
+                  ></img>
+                </button>
+              </Link>
             </div>
-        </section>
-    );
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default AboutCompany;
--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,23 +1,26 @@
 import React from "react";
-import "./Promo.css"
-import "../../vendor/fonts/fonts.css"
+import "./Promo.css";
+import "../../vendor/fonts/fonts.css";
 import Man from "../../images/main-man.png";
 
-
 function Promo() {
-
-    return (
-        <section className="promo">
-            <div className="promo__container">
-                <div className="promo__info">
-                    <h2 className="promo__title">Создавайте шедевры с помощью профессиональной металлообработки<br/> и сварки</h2>
-                    <p className="promo__subtitle">Изготовление и сварка металлоизделий любой сложности </p>
-                    <button className="promo__button">Заказать звонок</button>
-                </div>
-                <img src={Man} className="promo__image" alt="Фото сотрудника" />
-            </div>
-        </section>
-    )
+  return (
+    <section className="promo">
+      <div className="promo__container">
+        <div className="promo__info">
+          <h2 className="promo__title">
+            Создавайте шедевры с помощью профессиональной металлообработки
+            <br /> и сварки
+          </h2>
+          <p className="promo__subtitle">
+            Изготовление и сварка металлоизделий любой сложности
+          </p>
+          <button className="promo__button">Заказать звонок</button>
+        </div>
+        <img src={Man} className="promo__image" alt="Фото сотрудника" />
+      </div>
+    </section>
+  );
 }
 
 export default Promo;
