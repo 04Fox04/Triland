f3f904c7301079a5721fa0864adff4a83b5a4a21
Kirill <kirill.s.o@yandex.ru>
2023-09-27 10:15:53 +0300

    added the function of closing by overlay

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,13 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import "./Form.css";
 
 function Form({ onClose }) {
+  const formRef = useRef(null);
+
   useEffect(() => {
     //обработчик для клавиши "Esc"
     const handleEsc = (e) => {
+      //проверка на нажатие клавиши Esc (код клавиши 27)
       if (e.keyCode === 27) {
-        //проверка на нажатие клавиши Esc (код клавиши 27)
         onClose(); // вызывается функция закрытия попапа
       }
     };
@@ -21,35 +23,42 @@ function Form({ onClose }) {
     };
   }, [onClose]);
 
+  //обработчик клика на overlay
+  const handleOverlayClick = (e) => {
+    if (e.target === formRef.current) {
+      onClose(); //закрыть попап только если клик был на overlay
+    }
+  };
+
   return (
-    <div className="form">
-      <div className="form__container">
-        <button className="form__container-closed" onClick={onClose}></button>
-        <div className="form__container-titles">
-          <h2 className="form__container-title">Заказать обратный звонок</h2>
-          <p className="form__container-subtitle">
+    <div className="overlay" onClick={handleOverlayClick} ref={formRef}>
+      <form className="form">
+        <button className="form__closed" onClick={onClose}></button>
+        <div className="form__titles">
+          <h2 className="form__title">Заказать обратный звонок</h2>
+          <p className="form__subtitle">
             Закажите звонок и мы перезвоним Вам в течении 5 минут!
           </p>
         </div>
         <input
-          className="form__container-input"
+          className="form__input"
           type="text"
           placeholder="Имя"
         ></input>
         <input
-          className="form__container-input"
+          className="form__input"
           type="tel"
           placeholder="Телефон"
         ></input>
-        <button className="form__container-submit">Заказать звонок</button>
-        <p className="form__container-info">
+        <button className="form__submit">Заказать звонок</button>
+        <p className="form__info">
           Нажимая на эту кнопку вы соглашаетесь на
           <br />
-          <Link className="form__container-link">
+          <Link className="form__link">
             обработку персональных данных
           </Link>
         </p>
-      </div>
+      </form>
     </div>
   );
 }
