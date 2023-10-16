6569549e4317c9cacf442245297c6eeffb4cb587
Kirill <kirill.s.o@yandex.ru>
2023-10-16 12:05:40 +0300

    adaptive PhotoGallery page

--- src/components/PhotoGallery/PhotoGallery.js
+++ src/components/PhotoGallery/PhotoGallery.js
@@ -23,13 +23,13 @@ function PhotoGallery() {
   const handleLoadMore = () => {
     if (window.innerWidth <= 1200 && window.innerWidth >= 1000) {
       setDisplayedPhotos(displayedPhotos + 10); // если ширина экрана меньше 1200 добавляется еще 10 фотографий
-      setShowPhoto(70);
+      setShowPhoto(70); // кнопка дизейблится на 70 фотографиях 
     } else if (window.innerWidth <= 1000 && window.innerWidth >= 751) {
       setDisplayedPhotos(displayedPhotos + 8); // если ширина экрана меньше 1000 добавляется еще 8 фотографий
-      setShowPhoto(68);
+      setShowPhoto(68); // кнопка дизейблится на 68 фотографиях 
     } else if (window.innerWidth <= 750 && window.innerWidth >= 0) {
       setDisplayedPhotos(displayedPhotos + 6); // если ширина экрана меньше 750 добавляется еще 6 фотографий
-      setShowPhoto(64); // если ширина экрана меньше 750 добавляется еще 6 фотографий
+      setShowPhoto(64); // кнопка дизейблится на 64 фотографиях 
     } else {
       setDisplayedPhotos(displayedPhotos + 12); // по дефолту добалвяется 12 фотографий
     }
