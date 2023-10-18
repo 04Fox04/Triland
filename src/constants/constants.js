3350f8c26c0685278d14b945642adfccd92055dd
Kirill <kirill.s.o@yandex.ru>
2023-10-18 09:55:20 +0300

    fix regex for phone | fix form input  adaptation

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -106,8 +106,10 @@ import photoGalleryImage60 from "../images/photo-gallery/в2.jpg";
 import photoGalleryImage61 from "../images/photo-gallery/в3.jpg";
 import photoGalleryImage62 from "../images/photo-gallery/в4.jpg";
 import photoGalleryImage63 from "../images/photo-gallery/в5.jpg";
-export const nameRegex = /^[a-zA-Zа-яёЁА-Я\s]+$/; // разрешение только пробелов и букв
-export const telRegex = /^(?:\+[\d\s]+|[\d\s]+)$/; // телефон начинается с "+" и содержит цифры и пробелы, или состоит только из цифр и пробелов
+// разрешение только пробелов и букв
+export const nameRegex = /^[a-zA-Zа-яёЁА-Я\s]+$/;
+// телефон начинается с "+" и содержит цифры, пробелы, тире и скобки, или состоит только из цифр и пробелов. Номер телефона не может заканчиваться ()
+export const telRegex = /^(?!.*[-+()]$)(?:\+\d{1,3}\s?)?[\d\s()-]+$/; 
 
 export {
   metalProcessing,
