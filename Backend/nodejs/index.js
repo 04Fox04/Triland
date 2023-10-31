793bf9c36a14aee8ad86775ac054bf7e39471622
Klimanova Julia <sempppai124@yandex.ru>
2023-10-31 22:04:37 +0300

    test5 fix cors

--- Backend/nodejs/index.js
+++ Backend/nodejs/index.js
@@ -9,8 +9,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://xn--80aikmlon.xn--p1ai");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
