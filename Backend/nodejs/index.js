f4c23a4a3ca3bf2850313b627fe6a39c0318696e
Klimanova Julia <sempppai124@yandex.ru>
2023-10-31 16:13:03 +0300

    test2 fix cors

--- Backend/nodejs/index.js
+++ Backend/nodejs/index.js
@@ -8,13 +8,8 @@ import { body, validationResult } from 'express-validator'
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://xn--80aikmlon.xn--p1ai");
-  next();
-});
-
 app.use(cors({
-  origin: ['http://триланд.рф', 'https://триланд.рф', 'http://xn--80aikmlon.xn--p1ai', 'https://xn--80aikmlon.xn--p1ai'],
+  origin: ['http://триланд.рф', 'https://триланд.рф', 'http://xn--80aikmlon.xn--p1ai', 'https://xn--80aikmlon.xn--p1ai', 'https://xn----7sbkcsqcrxt.xn--p1ai'],
   methods: ['POST'],
   optionsSuccessStatus: 200,
 }));
